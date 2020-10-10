d6ba04964aaa3b67654d56d3dd5e3241765fd1a7
Elijah Brown <elijahbrown737@gmail.com>
2020-10-10 15:24:38 -0700

    styling and new image

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,7 @@
 var modal1 = document.getElementById("myModal1");
 var modal2 = document.getElementById("myModal2");
 var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
 // var img = document.getElementById("port1");
 // var modalImg = document.getElementById("img01");
 // var captionText = document.getElementById("caption");
@@ -17,9 +18,14 @@ function modal_img3(){
   modal3.style.display = "block";
 };
 
+function modal_img4(){
+  modal4.style.display = "block";
+};
+
 var span1 = document.getElementsByClassName("close1")[0];
 var span2 = document.getElementsByClassName("close2")[0];
 var span3 = document.getElementsByClassName("close3")[0];
+var span4 = document.getElementsByClassName("close4")[0];
 
 // When the user clicks on <span> (x), close the modal
 span1.onclick = function() { 
@@ -32,4 +38,8 @@ span2.onclick = function() {
 
 span3.onclick = function() { 
   modal3.style.display = "none";
+};
+
+span4.onclick = function() { 
+  modal4.style.display = "none";
 };
